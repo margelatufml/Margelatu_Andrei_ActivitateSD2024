2912d57427b9a7e931aca8a25ebb22a16d5316c6
Andrei Margelatu <84659334+margelatufml@users.noreply.github.com>
2024-03-14 11:01:14 +0200

    Update task2.c
    update task2

---
+++ Task Suplimentar/task2.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include<malloc.h>
+
+
+struct MagazinAutomobile {
+    int cod;
+    char* marca;
+    int nrModele;
+    float* preturiModele;
+};
+
+struct MagazinAutomobile citesteAutomobilMagazin() {
+    struct  MagazinAutomobile mag;
+    printf("Introduceti codul magazinului: ");
+    scanf("%d", &mag.cod);
+
+    mag.marca = malloc(50 * sizeof(char));
+    printf("Introduceti marca automobilului: ");
+    scanf("%s", mag.marca);
+
+    printf("Introduceti numarul de modele: ");
+    scanf("%d", &mag.nrModele);
+
+    mag.preturiModele = malloc(mag.nrModele * sizeof(float));
+    for (int i = 0; i < mag.nrModele; i++) {
+        printf("Introduceti pretul modelului %d: ", i + 1);
+        scanf("%f", &mag.preturiModele[i]);
+    }
+    return mag;
+}
+
+float calculeazaMediaPreturi(struct MagazinAutomobile mag) {
+    float suma = 0;
+    for (int i = 0; i < mag.nrModele; i++) {
+        suma += mag.preturiModele[i];
+    }
+    return suma / mag.nrModele;
+}
+
+void modificaMarca(struct MagazinAutomobile* mag, char* nouaMarca) {
+    free(mag->marca);
+    mag->marca = malloc(strlen(nouaMarca) + 1);
+    strcpy(mag->marca, nouaMarca);
+}
+
+void afiseazaAutomobilMagazin(struct MagazinAutomobile mag) {
+    printf("Cod Magazin: %d\n", mag.cod);
+    printf("Marca: %s\n", mag.marca);
+    printf("Numar Modele: %d\n", mag.nrModele);
+    for (int i = 0; i < mag.nrModele; i++) {
+        printf("Pretul Modelului %d: %.2f\n", i + 1, mag.preturiModele[i]);
+    }
+}
+
+struct MagazinAutomobile* copiazavector(int n, struct MagazinAutomobile* vector) {
+
+}
+
+
+
+int main() {
+    struct MagazinAutomobile magazin = citesteAutomobilMagazin();
+
+    //printf("Informatii initiale ale magazinului:\n");
+    //afiseazaAutomobilMagazin(magazin);
+
+    //float mediePreturi = calculeazaMediaPreturi(magazin);
+    //printf("Media preturilor este: %.2f\n", mediePreturi);
+
+    //modificaMarca(&magazin, "MarcaNoua");
+    //printf("Informatii magazin dupa modificarea marcii:\n");
+    //afiseazaAutomobilMagazin(magazin);
+
+    struct MagazinAutomobile* vector;
+    vector = (struct MagazinAutomobile*)malloc(5 * (sizeof(struct MagazinAutomobile) + 1));
+    printf("Citire Magazine pentru Automobile\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Magazinul %d\n", i + 1);
+        citesteAutomobilMagazin(&vector[i]);
+    }
+
+  
+
+
+    free(magazin.marca);
+    free(magazin.preturiModele);
+
+    return 0;
+}
