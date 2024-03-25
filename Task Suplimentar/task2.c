f38340af6c4f744be9e879ec6ee3e76f91f450a2
Andrei Margelatu <84659334+margelatufml@users.noreply.github.com>
2024-03-25 21:34:36 +0200

    Update task2.c

--- Task Suplimentar/task2.c
+++ Task Suplimentar/task2.c
@@ -1,8 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include<malloc.h>
-
 
 struct MagazinAutomobile {
     int cod;
@@ -12,18 +12,18 @@ struct MagazinAutomobile {
 };
 
 struct MagazinAutomobile citesteAutomobilMagazin() {
-    struct  MagazinAutomobile mag;
+    struct MagazinAutomobile mag;
     printf("Introduceti codul magazinului: ");
     scanf("%d", &mag.cod);
 
-    mag.marca = malloc(50 * sizeof(char));
+    mag.marca = (char*)malloc(50 * sizeof(char));
     printf("Introduceti marca automobilului: ");
     scanf("%s", mag.marca);
 
     printf("Introduceti numarul de modele: ");
     scanf("%d", &mag.nrModele);
 
-    mag.preturiModele = malloc(mag.nrModele * sizeof(float));
+    mag.preturiModele = (float*)malloc(mag.nrModele * sizeof(float));
     for (int i = 0; i < mag.nrModele; i++) {
         printf("Introduceti pretul modelului %d: ", i + 1);
         scanf("%f", &mag.preturiModele[i]);
@@ -41,7 +41,7 @@ float calculeazaMediaPreturi(struct MagazinAutomobile mag) {
 
 void modificaMarca(struct MagazinAutomobile* mag, char* nouaMarca) {
     free(mag->marca);
-    mag->marca = malloc(strlen(nouaMarca) + 1);
+    mag->marca = (char*)malloc(strlen(nouaMarca) + 1);
     strcpy(mag->marca, nouaMarca);
 }
 
@@ -54,39 +54,67 @@ void afiseazaAutomobilMagazin(struct MagazinAutomobile mag) {
     }
 }
 
-struct MagazinAutomobile* copiazavector(int n, struct MagazinAutomobile* vector) {
-
+struct MagazinAutomobile* creeazaVector(int n) {
+    struct MagazinAutomobile* vector = (struct MagazinAutomobile*)malloc(n * sizeof(struct MagazinAutomobile));
+    for (int i = 0; i < n; i++) {
+        vector[i] = citesteAutomobilMagazin();
+    }
+    return vector;
 }
 
+struct MagazinAutomobile* filtreazaAutomobile(struct MagazinAutomobile* vector, int n, int* nouaDimensiune, float pragPret) {
+    struct MagazinAutomobile* filtrat = (struct MagazinAutomobile*)malloc(n * sizeof(struct MagazinAutomobile));
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        if (calculeazaMediaPreturi(vector[i]) < pragPret) {
+            filtrat[j++] = vector[i];
+        }
+    }
+    *nouaDimensiune = j;
+    return filtrat;
+}
 
+struct MagazinAutomobile* concateneazaVectori(struct MagazinAutomobile* v1, int n1, struct MagazinAutomobile* v2, int n2) {
+    struct MagazinAutomobile* rezultat = (struct MagazinAutomobile*)malloc((n1 + n2) * sizeof(struct MagazinAutomobile));
+    for (int i = 0; i < n1; i++) {
+        rezultat[i] = v1[i];
+    }
+    for (int i = 0; i < n2; i++) {
+        rezultat[n1 + i] = v2[i];
+    }
+    return rezultat;
+}
+
+void afiseazaVector(struct MagazinAutomobile* vector, int n) {
+    for (int i = 0; i < n; i++) {
+        afiseazaAutomobilMagazin(vector[i]);
+    }
+}
 
 int main() {
-    struct MagazinAutomobile magazin = citesteAutomobilMagazin();
+    int nrAutomobile = 5;
+    struct MagazinAutomobile* magazine = creeazaVector(nrAutomobile);
 
-    //printf("Informatii initiale ale magazinului:\n");
-    //afiseazaAutomobilMagazin(magazin);
+    printf("Informatii initiale ale magazinului:\n");
+    afiseazaVector(magazine, nrAutomobile);
 
-    //float mediePreturi = calculeazaMediaPreturi(magazin);
-    //printf("Media preturilor este: %.2f\n", mediePreturi);
+    int nouaDimensiune;
+    struct MagazinAutomobile* magazineFiltrate = filtreazaAutomobile(magazine, nrAutomobile, &nouaDimensiune, 1000.00);
+    printf("Magazine filtrate:\n");
+    afiseazaVector(magazineFiltrate, nouaDimensiune);
 
-    //modificaMarca(&magazin, "MarcaNoua");
-    //printf("Informatii magazin dupa modificarea marcii:\n");
-    //afiseazaAutomobilMagazin(magazin);
-
-    struct MagazinAutomobile* vector;
-    vector = (struct MagazinAutomobile*)malloc(5 * (sizeof(struct MagazinAutomobile) + 1));
-    printf("Citire Magazine pentru Automobile\n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Magazinul %d\n", i + 1);
-        citesteAutomobilMagazin(&vector[i]);
+    // Eliberarea memoriei
+    for (int i = 0; i < nrAutomobile; i++) {
+        free(magazine[i].marca);
+        free(magazine[i].preturiModele);
     }
+    free(magazine);
 
-  
-
-
-    free(magazin.marca);
-    free(magazin.preturiModele);
+    for (int i = 0; i < nouaDimensiune; i++) {
+        free(magazineFiltrate[i].marca);
+        free(magazineFiltrate[i].preturiModele);
+    }
+    free(magazineFiltrate);
 
     return 0;
 }
