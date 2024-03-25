2f7327122d285ce68b7d1b9e5b6d90c632c7b2e7
Andrei Margelatu <84659334+margelatufml@users.noreply.github.com>
2024-03-25 21:10:27 +0200

    Update Seminar5.c

--- Seminar5.c
+++ Seminar5.c
@@ -1,1 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Biblioteca Biblioteca;
+typedef struct Nod Nod;
+
+struct Biblioteca {
+    char* nume;
+    int nrCarti;
+    int nrCititori;
+};
+
+struct Nod {
+    Biblioteca info;
+    Nod* next;
+};
+
+Biblioteca initializare(const char* nume, int nrCarti, int nrCititori) {
+    Biblioteca b;
+    b.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+    strcpy(b.nume, nume);
+    b.nrCarti = nrCarti;
+    b.nrCititori = nrCititori;
+    return b;
+}
+
+Nod* inserareInceput(Nod* cap, Biblioteca b) {
+    Nod* nou = (Nod*)malloc(sizeof(Nod));
+    nou->info = initializare(b.nume, b.nrCarti, b.nrCititori);
+    nou->next = cap;
+    return nou;
+}
+
+void afisareLista(Nod* cap) {
+    while (cap != NULL) {
+        printf("Biblioteca %s are %d carti si %d cititori.\n", cap->info.nume, cap->info.nrCarti, cap->info.nrCititori);
+        cap = cap->next;
+    }
+}
+
+char* getBibliotecaNrCartiPerCititor(Nod* cap) {
+    float max = 0;
+    char* nume_aux = NULL;
+
+    while (cap) {
+        if ((float)cap->info.nrCarti / cap->info.nrCititori > max) {
+            max = (float)cap->info.nrCarti / cap->info.nrCititori;
+            nume_aux = cap->info.nume;
+        }
+        cap = cap->next;
+    }
+
+    if (nume_aux == NULL) return NULL; 
+
+    char* nume = (char*)malloc(sizeof(char) * (strlen(nume_aux) + 1));
+    strcpy(nume, nume_aux);
+    return nume;
+}
+
+void InserareLaSfarsit(Nod** cap, Biblioteca b) {
+    Nod* sfarsit = (Nod*)malloc(sizeof(Nod));
+    sfarsit->info = initializare(b.nume, b.nrCarti, b.nrCititori);
+    sfarsit->next = NULL;
+
+    if (*cap != NULL) {
+        Nod* capA = *cap;
+        while (capA->next != NULL) {
+            capA = capA->next;
+        }
+        capA->next = sfarsit;
+    }
+    else {
+        *cap = sfarsit;
+    }
+}
+
+void stergeLista(Nod** cap) {
+    while (*cap != NULL) {
+        free((*cap)->info.nume);
+        Nod* copie = *cap;
+        *cap = (*cap)->next;
+        free(copie);
+    }
+}
+
+int main() { 
+
+    Nod* cap = NULL;
+
+    Biblioteca b1 = initializare("Mihai Eminescu", 150, 20);
+    cap = inserareInceput(cap, b1);
+
+    Biblioteca b2 = initializare("Ioan Slavici", 200, 30);
+    cap = inserareInceput(cap, b2);
+
+    Biblioteca b3 = initializare("Tudor Arghezi", 100, 15);
+    cap = inserareInceput(cap, b3);
+
+    afisareLista(cap);
+
+    char* numeDeAfisat = getBibliotecaNrCartiPerCititor(cap);
+    if (numeDeAfisat != NULL) { 
+        printf("Biblioteca cu media maxima: %s \n\n", numeDeAfisat);
+        free(numeDeAfisat);
+    }
+
+    Biblioteca b4 = initializare("Radu Tudoran", 100, 15); 
+    InserareLaSfarsit(&cap, b4);
+    afisareLista(cap);
+
+    stergeLista(&cap);
+    afisareLista(cap);
+
+    free(b1.nume);
+    free(b2.nume);
+    free(b3.nume);
+    free(b4.nume);
+
+    return 0; 
 
